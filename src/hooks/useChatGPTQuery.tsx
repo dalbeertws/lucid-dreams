4603a29d571f2af2501e485be421fb769d21139c
Inderpreet Singh <inders.tws@gmail.com>
2023-12-21 10:53:17 +0530

    have removed the errors by rewriting the gpt integration model and commenting the prev code

--- src/hooks/useChatGPTQuery.tsx
+++ src/hooks/useChatGPTQuery.tsx
@@ -1,37 +1,64 @@
 import { useQuery, QueryClient } from "@tanstack/react-query";
-import { OpenAI } from 'openai';
+import { OpenAI } from "openai";
 
+// export const useChatGPTQuery = (query: string) => {
+//   const queryClient = new QueryClient();
+//   const openai = new OpenAI({
+//     apiKey: process.env.OPENAI_API_KEY,
+//     dangerouslyAllowBrowser: true,
+//     // Add any other necessary configurations here
+//   });
+//   return useQuery<string, Error>({
+//     queryKey: ["chatGPT", query],
+//     queryFn: async () => {
+//       if (query === "") return ""; // If query is empty, return empty string (no need to call API)
+//       try {
+//         const completion = await openai.chat.completions.create({
+//           messages: [{ role: "system", content: query }],
+//           model: "gpt-3.5-turbo",
+//         });
+//         const data = completion.choices[0].message.content;
+//         console.log(completion, "completion"); // Log information if needed
+//         console.log(completion.choices[0], "data"); // Log information if needed
+//         return data; // Return the result
+//       } catch (error) {
+//         // Handle any errors that occur during the request
+//         throw new Error(String(error));
+//       }
+//     },
 
-
-
+//     enabled: !!query && typeof window !== "undefined", // Enable query only when query is not empty and in the browser context
+//     cacheTime: 0, // Disable caching to force API call each time
+//     queryClient, // Use the query client
+//   });
+// };
 
 export const useChatGPTQuery = (query: string) => {
-    const queryClient = new QueryClient();
-    const openai = new OpenAI({
-        apiKey: process.env.OPENAI_API_KEY,
-        dangerouslyAllowBrowser: true
-        // Add any other necessary configurations here
+//   const queryClient = new QueryClient();
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    dangerouslyAllowBrowser: true,
+    // Add any other necessary configurations here
+  });
+
+  const fetchData = async () => {
+    if (query === "") return "";
+
+    try {
+      const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: query }],
+        model: "gpt-3.5-turbo",
       });
-    return useQuery<string, Error>({
-        queryKey: ["chatGPT", query],
-        queryFn: async () => {
-            if (query === "") return ""; // If query is empty, return empty string (no need to call API)
-            try {
-                const completion = await openai.chat.completions.create({
-                    messages: [{ role: 'system', content: query }],
-                    model: 'gpt-3.5-turbo',
-                });
-                const data = completion.choices[0].message.content;
-                console.log(completion ,"completion"); // Log information if needed
-                console.log(completion.choices[0] ,"data"); // Log information if needed
-                return data; // Return the result
-            } catch (error) {
-                // Handle any errors that occur during the request
-                throw new Error(error); 
-            }
-        },
-        enabled: !!query && typeof window !== 'undefined', // Enable query only when query is not empty and in the browser context
-        cacheTime: 0, // Disable caching to force API call each time
-        queryClient, // Use the query client
-    });
+
+      const data = completion.choices[0]?.message.content || "";
+      return data;
+    } catch (error) {
+      throw new Error(String(error));
+    }
+  };
+  return useQuery<string, Error, string>({
+    queryKey: ["chatGPT", query],
+    queryFn: fetchData,
+    enabled: !!query && typeof window !== "undefined",
+  });
 };
